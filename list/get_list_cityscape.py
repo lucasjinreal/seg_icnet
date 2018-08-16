da8b740e81168ec78b81f8e3e8c548ee51e1b2c6
Your Name <you@example.com>
2018-08-16 14:40:32 +0800

    add get list script

---
+++ list/get_list_cityscape.py
@@ -0,0 +1,14 @@
+"""
+Get cityscape list for training
+"""
+import os
+
+import sys
+
+def get_pair(img_dir, label_dir):
+    if os.path.exists(img_dir) and os.path.exists(label_dir):
+        all_images = [i for i in os.listdir(img_dir) if i.endswith('png') or i.endswith('jpg') or i.endswith('jpeg')]
+        all_labels = [i for i in os.listdir(label_dir) if i.endswith('png') or i.endswith('jpg') or i.endswith('jpeg')]
+
+        # find the pair
+        pass
